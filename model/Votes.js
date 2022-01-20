c32d31d5db8f31386a18ce229e3c6c589d657038
Patrick Hagège <hagege.patrick@gmail.com>
2022-01-20 11:39:55 +0100

    comments editing only for creator s and some styling

---
+++ model/Votes.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const voteSchema = new Schema({
+	content: Number,
+    author: { type: Schema.Types.ObjectId, ref: "users" },
+    manga: { type: Schema.Types.ObjectId, ref: "mangas" }
+});
+
+const CommentModel = mongoose.model("votes", voteSchema);
+
+module.exports = CommentModel;
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const protectRoute = require("../middlewares/protectRoute");
 
 /* GET home page. */
-router.get("/profile", function (req, res, next) {
+router.get("/profile", protectRoute, function (req, res, next) {
   res.render("profile");
 });
 

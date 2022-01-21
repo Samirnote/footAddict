d63f823e79eca1710a281fd6a1a411613c2e0ab0
Djelouahi Samir <sam.djelouahi@gmail.com>
2022-01-21 09:01:50 +0100

    final done

--- model/manga.js
+++ model/manga.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 // structure of a manga document in the app / db
 const mangaSchema = new Schema({
     name: String,
-    category: ['Kodomo 子供', 'Shōnen 少年', 'Shōjo 少女', 'Seinen 青年', 'Josei 女性', 'Seijin 成人', 'Yaoi やおい', 'Yuri 百合'],
+    category: ['Gekiga 劇画', 'Hentai 変態', 'Mahō shōjo 魔法少女', 'Mecha メカ', 'Nekketsu 熱血', 'Yaoi やおい', 'Yuri 百合'],
     episodeNumber: Number,
     author: { type: Schema.Types.ObjectId, ref: "users" },
     cover: String,
